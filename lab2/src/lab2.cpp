7b533baed42d4af1918464691c2b5e181aa27a7d
manjula <mals13is@cmrit.ac.in>
2016-02-22 14:21:43 +0530

    First commit

--- lab2/src/lab2.cpp
+++ lab2/src/lab2.cpp
@@ -7,9 +7,43 @@
 //============================================================================
 
 #include <iostream>
+#include<fstream>
 using namespace std;
 
+class student{
+	string name,usn,branch,sem;
+	public:
+	void read();
+	void pack(char *fname);
+};
+void student::read()
+{
+			cout<<"enter the name";
+			cin>>name;
+			cout<<"enter the usn";
+			cin>>usn;
+			cout<<"enter the branch";
+			cin>>branch;
+			cout<<"enter the sem";
+			cin>>sem;
+
+}
+void student::pack(char *fname)
+{
+	fstream fp;
+	string buffer;
+	buffer=name+'|'+usn+'|'+branch+'|'+sem;
+	buffer.resize(100,'$');
+	fp.open(fname,ios::out);
+	fp<<buffer;
+	fp.close();
+}
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	student s;
+	char *fname;
+	cout<<"enter the filename";
+		cin>>fname;
+		s.read();
+		s.pack(fname);
 	return 0;
 }
