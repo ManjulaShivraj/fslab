64bcd83f5f0a8c162fc4f817cc7fc92527969749
Manjula Shivraj <mals13is@cmrit.ac.in>
2016-03-01 14:32:47 +0530

    existed commit

--- lab2/src/lab2.cpp
+++ lab2/src/lab2.cpp
@@ -1,5 +1,5 @@
 //============================================================================
-// Name        : lab2.cpp
+// Name        : fixlength.cpp
 // Author      : 
 // Version     :
 // Copyright   : Your copyright notice
@@ -16,6 +16,7 @@ class student{
 	void read();
 	void pack(string fname);
 	void unpack(string fname);
+	void search(string key,string filename);
 };
 void student::read()
 {
@@ -35,9 +36,9 @@ void student::pack(string fname)
 	string buffer;
 	buffer=name+'|'+usn+'|'+branch+'|'+sem;
 	buffer.resize(100,'$');
-	ofstream fp(fname.c_str(),ios::app);
-	fp<<buffer<<endl;
-	fp.close();
+	ofstream f1(fname.c_str(),ios::app);
+	f1<<buffer<<endl;
+	f1.close();
 }
 void student::unpack(string fname)
 {
@@ -50,25 +51,43 @@ void student::unpack(string fname)
 
 	f1.close();
 }
+void student::search(string key,string fname)
+{
+    student s[10];
+    int i=0,count=0;
+    ifstream f1(fname.c_str());
+    while(!f1.eof())
+    {
+        cout<<"in search";
+        s[i].unpack(f1);
+        if(key==s[i].usn)
+        {
+            cout<<"record found\n";
+            cout<<s[i].usn<<"\t"<<s[i].name;
+            break;
+
+        }
+        i++;
+    }
+    f1.close();
+}
 int main() {
 	student s;
 	string fname;
 	int choice;
-
 		cout<<"enter your choice"<<endl;
 		cin>>choice;
 		switch(choice)
 		{
 		case 1:
 		s.read();
-		cout<<"enter the filename";
-				cin>>fname;
+		cout<<"enter the filename"<<endl;
+		cin>>fname;
 		s.pack(fname);
 			   break;
 		case 2:
-			s.unpack(fname);
-				break;
+			cout<<"enter the usn"<<endl;
+			cin>>key;
+			s.search(key,fname);
+			break;
 		}
-		case
-	return 0;
-}
